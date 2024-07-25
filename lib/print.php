02e379dc480f2edb33d985092f3ed0a0f2ab8fff
bestdevelope <leabouillot7@gmail.com>
2024-07-25 13:19:08 +0200

    correction

--- lib/print.php
+++ lib/print.php
@@ -9,8 +9,8 @@ function print_title()
 }
 function print_description()
 {
-    if (isset($_GET['id'])) {
-        echo file_get_contents("data/" . $_GET['id']);
+    if(isset($_GET['id'])) {
+        echo file_get_contents("data/".$_GET['id']);
     } else {
         echo "Hello, PHP";
     }
@@ -21,7 +21,8 @@ function print_list()
     $i = 0;
     while ($i < count($list)) { //count() compte le list
         if ($list[$i] != "." && $list[$i] != "..") {
-            echo "<li><a href='index.php?id=$list[$i]'>$list[$i]</a></li>\n";
+            echo "<li><a 
+            href=\"index.php?id=$list[$i]\">$list[$i]</a></li>\n";
         }
         $i++;
     }
